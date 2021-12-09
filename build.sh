efa4f3d861ef87969dd9950e52c066e1d77f2d49
Jason Lee <jason@steeplesoft.com>
2021-12-09 15:43:32 -0600

    Script clean up Add logging

--- build.sh
+++ build.sh
@@ -1,19 +1,23 @@
 #/bin/bash
 
-#read -p "MySQL Host: " MYSQL_HOST
-#read -p "MySQL Database: " MYSQL_DB
-#read -p "MySQL User: " MYSQL_USER
-#read -p "MySQL Password: " MYSQL_PASS
+read -p "Server host (name or IP):" SERVER_HOST
+read -p "MySQL Host: " MYSQL_HOST
+read -p "MySQL Database: " MYSQL_DB
+read -p "MySQL User: " MYSQL_USER
+read -p "MySQL Password: " MYSQL_PASS
 
 function pause() {
     read -p "Press enter..."
 }
 
 BASE=`pwd`
-MYSQL_HOST=localhost
-MYSQL_DB=lego
-MYSQL_USER=lego
-MYSQL_PASS=legouniverse
+SERVER=$BASE/DarkflameServer
+AM=$BASE/AccountManager
+
+# MYSQL_HOST=localhost
+# MYSQL_DB=lego
+# MYSQL_USER=lego
+# MYSQL_PASS=legouniverse
 
 mysql -u root -p -h $MYSQL_HOST <<EOF
 drop database if exists $MYSQL_DB;
@@ -24,22 +28,26 @@ grant all on $MYSQL_DB.* to '$MYSQL_USER'@'$MYSQL_HOST';
 flush privileges;
 EOF
 
+# Install required deps. Not nearly complete yet.
 sudo dnf install zlib-devel unrar
 if [ ! -e $BASE/LEGO\ Universe\ \(unpacked\).rar ] ; then
+    echo Downloading client
     wget https://archive.org/download/lego-universe-unpacked/LEGO%20Universe%20%28unpacked%29.rar
 fi
 
 if [ ! -e $BASE/navmeshes.zip ] ; then
+    echo Downloading additional resources
     wget https://github.com/DarkflameUniverse/DarkflameServer/raw/main/resources/navmeshes.zip
 fi
 
+echo Cloning server source
 git clone --recursive https://github.com/DarkflameUniverse/DarkflameServer
+echo Cloning utils
 git clone https://github.com/lcdr/utils.git
+echo Cloning AccountManager
 git clone https://github.com/DarkflameUniverse/AccountManager.git
 
-SERVER=$BASE/DarkflameServer
-AM=$BASE/AccountManager
-
+echo "Building server..."
 cd $BASE/DarkflameServer
 mkdir build 
 cd build
@@ -47,17 +55,23 @@ cmake ..
 make
 pause
 
+echo Extracting resources from client archive
 unrar x $BASE/LEGO\ Universe\ \(unpacked\).rar res/macros res/BrickModels res/chatplus_en_us.txt res/names res/maps locale/locale.xml res/cdclient.fdb
-pause
-
 cd res
 unzip $BASE/navmeshes.zip
 pause
 
+echo Converstion database
 python3 $BASE/utils/utils/fdb_to_sqlite.py --sqlite_path CDServer.sqlite cdclient.fdb
+echo Updating SQLite database
+for SQL in  $SERVER/migrations/cdserver/0_nt_footrace.sql \
+    $SERVER/migrations/cdserver/1_fix_overbuild_mission.sql \
+    $SERVER/migrations/cdserver/2_script_component.sql ; do
+    sqlite3 $SERVER/build/res/CDServer.sqlite < $SQL
+done
 pause
 
-
+echo Applying database configuration
 cd $SERVER/build
 for INI in authconfig.ini  chatconfig.ini  masterconfig.ini  worldconfig.ini ; do
     sed -i "s/mysql_host=.*/mysql_host=$MYSQL_HOST/" $INI
@@ -67,18 +81,15 @@ for INI in authconfig.ini  chatconfig.ini  masterconfig.ini  worldconfig.ini ; d
 done
 pause
 
-
-for SQL in $SERVER/migrations/dlu/0_initial.sql \
-    $SERVER/migrations/cdserver/0_nt_footrace.sql \
-    $SERVER/migrations/cdserver/1_fix_overbuild_mission.sql \
-    $SERVER/migrations/cdserver/2_script_component.sql ; do
-    mysql --user=$MYSQL_USER --password=$MYSQL_PASS -h $MYSQL_HOST $MYSQL_DB < $SQL
-done
+echo Updating MySQL/MariaDB database
+mysql --user=$MYSQL_USER --password=$MYSQL_PASS -h $MYSQL_HOST $MYSQL_DB < $SERVER/migrations/dlu/0_initial.sql
 pause
 
+echo Creating admin user
 cd $SERVER/build
 ./MasterServer -a
 
+echo Installing required deps for AccountManager
 cd $AM
 for PKG in `cat requirements.txt` ; do 
     #pip3 install --user $PKG
@@ -86,8 +97,8 @@ for PKG in `cat requirements.txt` ; do
 done
 pip3 install --user $PKGS
 
+echo Writing AccountManager configuration
 KEY=`echo $RANDOM | sha256sum | head -32 | cut -f 1 -d ' '`
-
 echo "# credentials.py
 
 # Make sure this is a long random string
@@ -96,3 +107,18 @@ SECRET_KEY = '$KEY'
 # Replace instances of <> with the database credentials
 DB_URL = 'mysql+pymysql://$MYSQL_USER:$MYSQL_PASS@$MYSQL_HOST/$MYSQL_DB'
 " > credentials.py
+
+echo "# resources.py
+
+# Path to the logo image to display on the application
+LOGO = 'logo/logo.png'
+
+# Path to the privacy policy users have to agree to
+PRIVACY_POLICY = 'policy/Privacy Policy.pdf'
+
+# Path to the terms of use users have to agree to
+TERMS_OF_USE = 'policy/Terms of Use.pdf'" > resources.py
+
+echo Build finished.
+echo The server is available in $SERVER/build/MasterServer
+echo The AccountManager is available in $AM
